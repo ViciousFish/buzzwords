f868358e3bce3e569f6ae7483b468e1283d15491
Chuck Dries <chuck@chuckdries.com>
2021-06-27 10:38:34 -0700

    draw Buzz in react three fiber

--- client-vite/src/App.tsx
+++ client-vite/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
-import Buzz from "./Components/Zdog/Buzz";
+// import Buzz from "./Components/Zdog/Buzz";
+
 import { Canvas } from "@react-three/fiber";
 import { Html, Stats, useProgress } from "@react-three/drei";
 import CameraControls from "./Components/three/CameraControls";
 import HexWord from "./Components/three/HexWord";
+import { Buzz } from "./Components/three/Buzz";
 
 /* Three TODO
 - hexagon
@@ -18,35 +20,43 @@ import HexWord from "./Components/three/HexWord";
 function App() {
   console.log(window.devicePixelRatio);
   const { progress } = useProgress();
-  const dpr = (<>{window.devicePixelRatio} - {Math.max(window.devicePixelRatio, 2)}</>)
+  const dpr = (
+    <>
+      {window.devicePixelRatio} - {Math.max(window.devicePixelRatio, 2)}
+    </>
+  );
   return (
     <div className="App">
       <header className="App-header">
         {!import.meta.env.PROD && dpr}
-        <Buzz />
+        {/* <Buzz /> */}
         {/* <div > */}
         <Canvas
           style={{
             touchAction: "none",
             margin: "1em",
-            height: 300,
+            height: 500,
             minWidth: 600,
           }}
           camera={{
             position: [0, 0, 100],
             zoom: 20,
           }}
+          shadows
           orthographic
           dpr={Math.max(window.devicePixelRatio, 2)}
           flat
         >
+          <Buzz position={[0, 5, 0]} />
           {!import.meta.env.PROD && <Stats />}
           {/* <CameraControls /> */}
           <ambientLight />
           <directionalLight position={[10, 10, 10]} />
           <React.Suspense fallback={<Html center>{progress} % loaded</Html>}>
-            <HexWord position={[0, 2.4, 0]} text="COMING" />
-            <HexWord position={[0, -2.4, 0]} text="SOON!" />
+            <group position={[0, -2, 0]}>
+              <HexWord position={[0, 0, 0]} text="COMING" />
+              <HexWord position={[0, -4.8, 0]} text="SOON!" />
+            </group>
           </React.Suspense>
         </Canvas>
         {/* </div> */}
