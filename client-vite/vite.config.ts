9f7a33ebc7a0060c6f7e1466b3c9477a48b069c9
Chuck Dries <chuckd@axosoft.com>
2021-06-30 11:12:12 -0700

    move rollup plugin to top level vite plugins field

--- client-vite/vite.config.ts
+++ client-vite/vite.config.ts
@@ -5,25 +5,29 @@ import reactRefresh from "@vitejs/plugin-react-refresh";
 export default ({ command, mode }) => {
   console.log(mode);
   return defineConfig({
-    plugins: [reactRefresh()],
+    plugins: [
+      {
+        name: "replace-code",
+        transform(code, id) {
+          if (!/nbind/.test(id)) return;
+          console.log("replace code");
+          code = code.replace(
+            "_a = _typeModule(_typeModule),",
+            "var _a = _typeModule(_typeModule);"
+          );
+          return {
+            code,
+            map: { mappings: "" },
+          };
+        },
+      },
+      reactRefresh(),
+    ],
     build: {
       sourcemap: mode !== "production",
-      rollupOptions: {
-        plugins: [
-          {
-            name: 'replace-code',
-            transform (code, id) {
-              if (!/nbind/.test(id)) return
-              console.log('replace code')
-              code = code.replace('_a = _typeModule(_typeModule),', 'var _a = _typeModule(_typeModule);')
-              return {
-                code,
-                map: { mappings: '' }
-              }
-            }
-          }
-      ]
-      }
+      // rollupOptions: {
+      //   plugins: [],
+      // },
     },
   });
 };
