2b370bb72f5848a2c2c6ec98bff3735d99e10624
James Quigley <james@quigley.us>
2021-12-19 15:19:44 -0500

    do random letters following a distribution

--- shared/alphaHelpers.ts
+++ shared/alphaHelpers.ts
@@ -30,6 +30,22 @@ const letters = [
   "z",
 ];
 
+const probabilities = [
+  8.4966, 2.072, 4.5388, 3.3844, 11.1607, 1.8121, 2.4705, 3.0034, 7.5448,
+  0.1965, 1.1016, 5.4893, 3.0129, 6.6544, 7.1635, 3.1671, 0.1962, 7.5809,
+  5.7351, 6.9509, 3.6308, 1.0074, 1.2899, 0.2902, 1.7779, 0.2722,
+].map((p) => p / 100);
+
+let cdfArray: number[] = [];
+
+let total = 0;
+for (let p of probabilities) {
+  total += p;
+  cdfArray.push(total);
+}
+
+cdfArray = cdfArray.map((p) => Math.round(p * 100000) / 100000);
+
 export const isValidWord = (word: string): boolean => {
   if (word.length < 3 || !words.includes(word.toLowerCase())) {
     return false;
@@ -39,5 +55,14 @@ export const isValidWord = (word: string): boolean => {
 };
 
 export const getRandomCharacter = (): string => {
-  return letters[Math.floor(Math.random() * letters.length)];
+  let n = Math.random();
+  if (n > 0 && n < cdfArray[0]) {
+    return letters[0];
+  }
+  for (let i = 0; i < cdfArray.length - 1; i++) {
+    if (n > cdfArray[i] && n < cdfArray[i + 1]) {
+      return letters[i + 1];
+    }
+  }
+  return letters[letters.length - 1];
 };
