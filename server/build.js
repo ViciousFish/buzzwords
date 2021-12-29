4f04593a8aa31e344d125f5296db9be51c806c65
Chuck Dries <chuck@chuckdries.com>
2021-12-29 05:21:28 -0700

    Dockerize backend and add build scripts to workspace root

--- server/build.js
+++ server/build.js
@@ -5,6 +5,7 @@ require("esbuild") // eslint-disable-line
     platform: "node",
     outfile: "./dist/index.js",
     sourcemap: true,
+    watch: process.env.BUZZ_BUILD_WATCH || false,
     // plugins: require("./plugins"),
   })
   .catch(() => process.exit(1));
