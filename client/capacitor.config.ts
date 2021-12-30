ed3d1d5359c729d953574f0c751e16c544945d4a
Chuck Dries <chuck@chuckdries.com>
2021-12-30 10:46:35 -0800

    ios app working!

---
+++ client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chuckdries.buzzwords',
+  appName: 'Buzzwords',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
