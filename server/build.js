49342655adb899d352062a357fccb396df3947cc
James Quigley <james@quigley.us>
2022-01-18 20:32:13 -0800

    mark all honeycomb stuff as external

--- server/build.js
+++ server/build.js
@@ -6,6 +6,23 @@ require("esbuild") // eslint-disable-line
     outfile: "./dist/index.js",
     sourcemap: true,
     watch: Boolean(process.env.BUZZ_BUILD_WATCH) || false,
+    external: [
+      "http",
+      "express",
+      "mongodb",
+      "@opentelemetry/api",
+      "@opentelemetry/core",
+      "@opentelemetry/sdk-trace-node",
+      "@opentelemetry/sdk-trace-base",
+      "@opentelemetry/instrumentation-http",
+      "@opentelemetry/instrumentation-express",
+      "@opentelemetry/instrumentation-mongodb",
+      "@opentelemetry/exporter-trace-otlp-grpc",
+      "@opentelemetry/instrumentation",
+      "@grpc/grpc-js",
+      "@opentelemetry/resources",
+      "@opentelemetry/semantic-conventions",
+    ],
     // plugins: require("./plugins"),
   })
   .catch(() => process.exit(1));
