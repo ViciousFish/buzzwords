c4afdb2e2514795f63af4e78c8998049eee5e098
Chuck Dries <chuck@chuckdries.com>
2022-01-23 20:07:35 -0800

    working dictionary stuff

--- client/src/features/play-route/MoveListItem.tsx
+++ client/src/features/play-route/MoveListItem.tsx
@@ -1,11 +1,12 @@
 import {
   faBook,
+  faExclamationTriangle,
   faPlay,
   faPlayCircle,
   faSpinner,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { Move } from "buzzwords-shared/Game";
 import classNames from "classnames";
 import React, { useCallback, useState } from "react";
@@ -31,29 +32,76 @@ const MoveListItem: React.FC<MoveListItemProps> = ({ move, index }) => {
   );
 
   const [isOpen, setIsOpen] = useState(false);
-  const [dictionaryData, setDictionaryData] = useState(null);
+  const [dictionaryData, setDictionaryData] = useState(null as any | null);
 
-  const openPopover = useCallback(() => {
+  const openPopover = useCallback(async () => {
     setIsOpen(true);
-    axios
-      .get(
+    try {
+      const res = await axios.get(
         `https://api.dictionaryapi.dev/api/v2/entries/en/${move.letters.join(
           ""
         )}`,
         { withCredentials: false }
-      )
-      .then((res) => {
-        setDictionaryData(res.data);
-        console.log("res.data", res.data);
+      );
+      setDictionaryData(res.data);
+    } catch (e) {
+      console.log(Object.keys(e))
+      setDictionaryData({
+        type: "error",
+        status: e.response?.status
       });
+    }
   }, [setIsOpen, move.letters]);
 
   const popoverContent = (
-    <div className="bg-primary rounded-xl px-4 py-2 w-[250px]">
-      <div className="flex justify-end">
+    <div className="bg-primary rounded-xl px-4 py-2 w-[300px] z-30 shadow-lg">
+      <div className="flex justify-between items-baseline">
+        <FontAwesomeIcon className="mr-2" icon={faBook} />
+        <span className="capitalize text-4xl font-bold mr-2 font-serif">
+          {move.letters.join("")}
+        </span>
+        <span className="flex-auto"></span>
+        {move.date && (
+          <span className="text-xs">{relativeDate(move.date)}</span>
+        )}
+      </div>
+
+      {dictionaryData ? (
+        <>
+          {dictionaryData.type === "error" && (
+            <div className="flex justify-center p-4 gap-1 items-center">
+              <FontAwesomeIcon icon={faExclamationTriangle} /> {dictionaryData.status}
+            </div>
+          )}
+          {dictionaryData.type !== "error" && (
+            <ul>
+              {/* @ts-ignore */}
+              {dictionaryData[0].meanings.map((meaning, index) => {
+                console.log("meaning", meaning);
+                return (
+                  <li key={index}>
+                    <p className="font-serif inline-block">
+                      <span className="italic mr-2 opacity-50">
+                        {meaning.partOfSpeech}
+                      </span>
+                      {meaning.definitions
+                        .map((def) => def.definition)
+                        .join(" ")}
+                    </p>
+                  </li>
+                );
+              })}
+            </ul>
+          )}
+        </>
+      ) : (
+        <div className="flex justify-center p-4">
+          <FontAwesomeIcon className="animate-spin" icon={faSpinner} />
+        </div>
+      )}
         <Button
           className={classNames(
-            "bg-darkbrown text-white text-sm p-2 m-0 rounded-md",
+            "bg-darkbrown text-white text-sm p-2 m-0 w-full",
             replayState && currentReplayIndex === index && "bg-blue-400 "
           )}
           onClick={() => {
@@ -71,38 +119,13 @@ const MoveListItem: React.FC<MoveListItemProps> = ({ move, index }) => {
           />
           replay
         </Button>
-      </div>
-      <div className="flex justify-between items-baseline">
-        <FontAwesomeIcon className="mr-2" icon={faBook} />
-        <span className="capitalize text-xl font-bold mr-2">
-          {move.letters.join("")}
-        </span>
-        <span className="flex-auto"></span>
-        {move.date && (
-          <span className="text-sm">{relativeDate(move.date)}</span>
-        )}
-      </div>
-
-      {dictionaryData ? (
-        <>
-          {JSON.stringify(
-            dictionaryData[0].meanings.map((meaning) => [
-              meaning.partOfSpeech,
-              meaning.definitions.map((def) => def.definition),
-            ])
-          )}
-        </>
-      ) : (
-        <div className="flex justify-center p-4">
-          <FontAwesomeIcon className="animate-spin" icon={faSpinner} />
-        </div>
-      )}
     </div>
   );
   return (
     <Popover
+      containerClassName="z-30"
       isOpen={isOpen}
-      positions={["left"]}
+      positions={["left", "top", "bottom"]}
       content={popoverContent}
       onClickOutside={() => setIsOpen(false)}
     >
