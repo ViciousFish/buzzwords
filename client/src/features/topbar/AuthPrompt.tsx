fe88f541bb8f8cc8993ebab7513f0b21c1af6f08
Chuck Dries <chuck@chuckdries.com>
2022-02-17 17:56:13 -0800

    Add login form to auth prompt

--- client/src/features/topbar/AuthPrompt.tsx
+++ client/src/features/topbar/AuthPrompt.tsx
@@ -2,14 +2,84 @@ import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames";
-import React from "react";
+import { ErrorMessage, Field, Formik } from "formik";
+import React, { useState } from "react";
+import * as yup from "yup";
+
 import { getApiUrl } from "../../app/apiPrefix";
+import Button from "../../presentational/Button";
+
+const AuthRegister: React.FC = () => {};
+
+const LoginValidationSchema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(6).required(),
+});
+
+const AuthLogin: React.FC = () => {
+  return (
+    <>
+      <h3 className="text-lg font-bold m-0">Login</h3>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={LoginValidationSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          console.log(values.email);
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <div className="min-w-[300px]">
+            <div className="flex flex-col">
+              <label htmlFor="login-email">Email</label>
+              <Field
+                className="block rounded p-2 bg-white bg-opacity-75"
+                id="login-email"
+                type="email"
+                name="email"
+              />
+              <ErrorMessage className='opacity-75 text-sm' name="email" component="div" />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="login-password">Password</label>
+              <Field
+                className="block rounded p-2 bg-white bg-opacity-75"
+                id="login-password"
+                type="password"
+                name="password"
+              />
+              <ErrorMessage className='opacity-75 text-sm' name="password" component="div" />
+            </div>
+            <div className="flex justify-center mt-2">
+              <Button
+                variant="dark"
+                className="text-sm flex-auto"
+                disabled={isSubmitting}
+              >
+                Login
+              </Button>
+            </div>
+          </div>
+        )}
+      </Formik>
+    </>
+  );
+};
 
 interface AuthPromptProps {
   onDismiss: () => void;
 }
 
+type AuthPromptView =
+  | "login-options"
+  | "user-pass-login"
+  | "user-pass-register";
+
 const AuthPrompt: React.FC<AuthPromptProps> = ({ onDismiss }) => {
+  const [view, setView] = useState<AuthPromptView>("login-options");
   return (
     <div className="rounded-xl border border-darkbrown bg-primary shadow-lg p-4">
       <button
@@ -19,24 +89,52 @@ const AuthPrompt: React.FC<AuthPromptProps> = ({ onDismiss }) => {
       >
         <FontAwesomeIcon icon={faTimes} />
       </button>
-      <h3 className="text-lg font-bold m-0">Link an account</h3>
-      <p>to sync your games across devices</p>
-      <div className="my-2">
-        <a
-          className={classNames(
-            "block bg-darkbrown text-white p-2 text-sm hover:bg-opacity-50",
-            "rounded-full inset-shadow transition-all"
-          )}
-          href={getApiUrl("/login/google")}
-        >
-          <FontAwesomeIcon className="mx-2" icon={faGoogle} /> Sign in with
-          Google
-        </a>
-      </div>
-      <p className="px-2 text-xs opacity-75 max-w-xs">
-        Don&apos;t worry, the game will continue to work just fine if you choose
-        not to.
-      </p>
+      {view === "login-options" && (
+        <>
+          <h3 className="text-lg font-bold m-0">Link an account</h3>
+          <p>to sync your games across devices</p>
+          <a
+            className={classNames(
+              "mt-2 block bg-darkbrown text-white p-2 text-sm hover:bg-opacity-50",
+              "rounded-full inset-shadow transition-all"
+            )}
+            href={getApiUrl("/login/google")}
+          >
+            <FontAwesomeIcon className="mx-2" icon={faGoogle} /> Sign in with
+            Google
+          </a>
+          <div className="my-2 flex space-x-2">
+            <button
+              type="button"
+              onClick={() => setView("user-pass-login")}
+              className="flex-auto bg-darkbrown text-white p-2 text-sm hover:bg-opacity-50 rounded-full inset-shadow transition-all"
+            >
+              Log in with email
+            </button>
+            <button className="flex-auto bg-darkbrown text-white p-2 text-sm hover:bg-opacity-50 rounded-full inset-shadow transition-all">
+              Register with email
+            </button>
+          </div>
+          <p className="px-2 text-xs opacity-75 max-w-xs">
+            Don&apos;t worry, the game will continue to work just fine if you
+            choose not to.
+          </p>
+        </>
+      )}
+      {view === "user-pass-login" && (
+        <>
+          <button
+          className="underline text-sm"
+            onClick={() => {
+              setView("login-options");
+            }}
+            type="button"
+          >
+            back
+          </button>
+          <AuthLogin />
+        </>
+      )}
     </div>
   );
 };
