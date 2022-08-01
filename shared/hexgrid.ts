1dd10605e0b6db27e8a73aff1f19850e9b874ff3
James Quigley <james@quigley.us>
2022-08-01 09:28:26 -0400

    More commenting

--- shared/hexgrid.ts
+++ shared/hexgrid.ts
@@ -117,6 +117,8 @@ export const getPotentialWords = (
 };
 
 /**
+ * Returns the letters that should appear on the tiles about to be reset.
+ *
  * Iterate over the words dictionary, finding words that
  * could be made using existing tiles on the board
  * AND the new tiles we're about to reset.
@@ -138,6 +140,17 @@ export const getNewCellValues = (
 
   let words = Object.keys(wordsJson);
 
+  /**
+   * Accomplishes two steps at once:
+   * 1. Filter for words that could be made now, or with the introduction our reset tiles
+   * 2. Determine what letters would need to be added to the board in order to make the word
+   *
+   * Returns an object like
+   *
+   * {
+   *  <word>: <letters needed>[]
+   * }
+   */
   const wordsToMissingLetters = words.reduce((acc, word) => {
     const wordLetters = word.split("");
     for (let letter of lettersOnTheBoard) {
@@ -163,9 +176,14 @@ export const getNewCellValues = (
 
   const neededLetters = wordsToMissingLetters[word];
 
+  /**
+   * If we can already make the word with letters on the board
+   * Or with a less than resetTileTotal, we need "random" letters as filler.
+   * This picks random characters as filler, but won't pick letters that
+   * appear more than MAX_REPEATED_LETTER times.
+   */
   const fillerLength = resetTileTotal - neededLetters.length;
   let filler: string[] = [];
-
   for (let i = 0; i < fillerLength; i++) {
     const letterCount = R.countBy(R.identity, [
       ...lettersOnTheBoard,
