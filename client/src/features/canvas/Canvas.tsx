04db08955cedd84656178ce1c4ceb70c300bd279
Chuck Dries <chuck@chuckdries.com>
2022-08-11 17:28:08 -0700

    upgrade three and solve warnings

--- client/src/features/canvas/Canvas.tsx
+++ client/src/features/canvas/Canvas.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { Canvas as ThreeCanvas } from "@react-three/fiber";
 import { useContextBridge } from "@react-three/drei";
 import { ReactReduxContext } from "react-redux";
@@ -6,17 +6,23 @@ import { ReactReduxContext } from "react-redux";
 import Wrap3d from "./Wrap3d";
 import classNames from "classnames";
 
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+import { extend } from '@react-three/fiber'
+
+
 const Canvas: React.FC<{
   className?: string;
+  children: ReactNode;
 }> = ({ children, className }) => {
   const ReduxProvider = useContextBridge(ReactReduxContext);
+  extend({ TextGeometry })
 
   return (
     <ThreeCanvas
       className={classNames("shrinkable", className)}
       camera={{
         position: [0, 0, 20],
-        zoom: 3,
+        zoom: 5,
       }}
       gl={{
         powerPreference: "low-power",
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,7 +10,7 @@ import "./index.css";
 import App from "./app/App";
 
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 
 root.render(
   <React.StrictMode>
