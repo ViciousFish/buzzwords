4abc938893c39558428449105611a03514b6db03
Chuck Dries <chuck@chuckdries.com>
2022-08-11 17:00:28 -0700

    upgrade react

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 
 import "react-toastify/dist/ReactToastify.css";
 
@@ -9,11 +9,13 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./app/App";
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
