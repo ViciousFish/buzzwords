6a27d5c76d3d975c21b818a364691a90f900338f
James Quigley <james@quigley.us>
2022-08-13 18:49:25 -0400

    Wip more builds

--- client/scripts/notarize.js
+++ client/scripts/notarize.js
@@ -1,21 +1,24 @@
 // https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
-require('dotenv').config();
-const { notarize } = require('electron-notarize');
+require("dotenv").config();
+const { notarize } = require("electron-notarize");
 
 exports.default = async function notarizing(context) {
-  const { electronPlatformName, appOutDir } = context;  
-  if (electronPlatformName !== 'darwin') {
+  if (!process.env.CSC_NAME) {
+    return;
+  }
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== "darwin") {
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
 
   return await notarize({
-    tool: 'notarytool',
-    appBundleId: 'com.gg.buzzwords.desktop',
+    tool: "notarytool",
+    appBundleId: "com.gg.buzzwords.desktop",
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEIDPASS,
-    teamId:  process.env.APPLETEAMID
+    teamId: process.env.APPLETEAMID,
   });
 };
