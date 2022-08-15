a2d3c9b1e2e814d47bd846cf31d2cda7faba5dc5
Chuck Dries <chuck@chuckdries.com>
2022-08-14 22:02:26 -0700

    fix broken tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: "jit",
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
