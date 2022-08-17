76202f4afb93461c8e2ae1571fbe70275a2256e5
Chuck Dries <chuck@chuckdries.com>
2022-08-16 19:12:21 -0700

    set bundle ID

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.gg.buzzwords.mobile",
+  appId: "gg.buzzwords.htmlclient",
   appName: "Buzzwords",
   webDir: "dist",
   bundledWebRuntime: false,
