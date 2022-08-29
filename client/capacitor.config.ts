bc06fa5187d88b40230dbd7f80e5596c698f1f04
Chuck Dries <chuck@chuckdries.com>
2022-08-29 05:01:58 -0700

    update bundle ID and add associated domains to ios app

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "gg.buzzwords.htmlclient",
+  appId: "gg.buzzwords.mobile",
   appName: "Buzzwords",
   webDir: "dist",
   bundledWebRuntime: false,
