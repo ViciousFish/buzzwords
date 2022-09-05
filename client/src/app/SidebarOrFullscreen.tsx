323bdfb91770a75a9243eb3be642095d3655b697
Chuck Dries <chuck@chuckdries.com>
2022-09-05 13:25:56 -0700

    WIPing tutorial pane

--- client/src/app/SidebarOrFullscreen.tsx
+++ client/src/app/SidebarOrFullscreen.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import { createPortal } from 'react-dom';
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { createPortal } from "react-dom";
 
 interface SidebarOrFullscreenProps {
   mobileLayout: boolean;
@@ -7,7 +9,18 @@ interface SidebarOrFullscreenProps {
 }
 
 const SidebarOrFullscreen = ({}: SidebarOrFullscreenProps) => {
-  return <div className="bg-blue-400 w-[300px]">hello</div>;
-}
+  return (
+    <div className="w-[300px] border-l border-black">
+      <div className="h-[50px] bg-primary text-xl flex items-center justify-between px-4">
+        <span>Tutorial</span>
+        <FontAwesomeIcon icon={faTimes} />
+      </div>
+      <div className="p-2">
+
+      <h3>Welcome to Buzzwords!</h3>
+      </div>
+    </div>
+  );
+};
 
 export default SidebarOrFullscreen;
